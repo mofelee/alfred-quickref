2eccb892320ae90bac5c200ec0c9d27be1f2f80c
Mofe Lee <mofe@me.com>
2022-11-17 20:44:55 +0800

    support section serach

--- index.js
+++ index.js
@@ -1,6 +1,68 @@
 const Fuse = require("fuse.js");
-const list = require("./list.json");
-let input = process.argv[2];
+const data = require("./data.json");
+let input = process.argv[2] || "";
+
+const searchData = data.map((v) => {
+  v.key = v.path.replace(/docs\/(.+)\.html$/, "$1");
+  return v;
+});
+
+if (input === "") {
+  output(searchData.map((item) => mapOutputItem(item)));
+  process.exit(0);
+}
+
+const options = {
+  //includeScore: true,
+  keys: [
+    {
+      name: "key",
+      weight: 0.6,
+    },
+    {
+      name: "title",
+      weight: 0.2,
+    },
+    {
+      name: "intro",
+      weight: 0.2,
+    },
+  ],
+};
+
+const fuse = new Fuse(searchData, options);
+
+const result = fuse.search(input);
+
+output(result.map(({ item }) => mapOutputItem(item)));
+
+function mapOutputItem(item) {
+  return {
+    valid: true,
+    title: `【${item.key}】${item.title}`,
+    subtitle: item.intro,
+    arg: item.key,
+    variables: {
+      MODE: "openpage",
+      TOPICPATH: item.path,
+      TOPICHASH: "",
+    },
+    mods: {
+      cmd: {
+        valid: true,
+        title: `【${item.key}】${item.title}`,
+        arg: item.key,
+        subtitle: `检索【${item.key}】的章节`,
+        variables: {
+          MODE: "sectionsearch",
+          TOPICPATH: item.path,
+          TOPICLEVEL: 2,
+          TOPICKEY: item.key,
+        },
+      },
+    },
+  };
+}
 
 function output(items) {
   console.log(
@@ -10,12 +72,3 @@ function output(items) {
     })
   );
 }
-const options = {
-  //includeScore: true,
-};
-
-const fuse = new Fuse(list, options);
-
-const result = fuse.search(input);
-
-output(result.map((v) => ({ title: v.item, arg: v.item })));
---
+++ section.js
@@ -0,0 +1,101 @@
+const Fuse = require("fuse.js");
+const data = require("./data.json");
+let input = process.argv[2] || "";
+
+const TOPICPATH = process.env.TOPICPATH;
+const TOPICKEY = process.env.TOPICKEY;
+const TOPICLEVEL = process.env.TOPICLEVEL;
+const TOPICHASH = process.env.TOPICHASH;
+const TOPICPARENTTITLE = process.env.TOPICPARENTTITLE || "";
+
+const level = parseInt(TOPICLEVEL, 10);
+
+const index = data.findIndex((v) => v.path == TOPICPATH);
+
+if (index == -1) {
+  output([{ title: "参数有误" }]);
+  process.exit(0);
+}
+
+const topic = data[index];
+
+let sections = [];
+if (level === 2) {
+  sections = topic.sections.filter((v) => v.l === 2);
+} else {
+  // level === 3
+
+  const startIndex = topic.sections.findIndex((v) => v.a === TOPICHASH);
+  for (let i = startIndex + 1; i < topic.sections.length; i++) {
+    const section = topic.sections[i];
+    if (section.l === 2) {
+      break;
+    }
+    sections.push(section);
+  }
+}
+
+if (input === "") {
+  output(sections.map((item) => mapOutputItem(item)));
+  process.exit(0);
+}
+
+const options = {
+  //includeScore: true,
+  keys: [
+    {
+      name: "t",
+      weight: 1,
+    },
+  ],
+};
+
+const fuse = new Fuse(sections, options);
+
+const result = fuse.search(input);
+
+output(result.map(({ item }) => mapOutputItem(item)));
+
+function mapOutputItem(item) {
+  title = `【${TOPICKEY}${TOPICPARENTTITLE ? ` > ${TOPICPARENTTITLE}` : ""}}】${
+    item.t
+  } ${level === 2 ? ">" : ""}`;
+
+  const mods = {};
+  if (level === 2) {
+    mods.cmd = {
+      valid: true,
+      title,
+      arg: TOPICKEY,
+      subtitle: `检索【${TOPICKEY} > ${item.t}】的章节`,
+      variables: {
+        MODE: "sectionsearch",
+        TOPICHASH: item.a,
+        TOPICLEVEL: 3,
+        TOPICPARENTTITLE: item.t,
+        TOPICKEY: item.key,
+      },
+    };
+  }
+
+  return {
+    title,
+    subtitle: item.t,
+    arg: TOPICKEY,
+    variables: {
+      MODE: "openpage",
+      TOPICHASH: item.a,
+      TOPICKEY: item.key,
+    },
+    mods,
+  };
+}
+
+function output(items) {
+  console.log(
+    JSON.stringify({
+      return: 0.1,
+      items,
+    })
+  );
+}
